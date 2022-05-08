ed2be4747c9f82722e8d7daaeb26c465c78b840c
Hju95 <remit_@naver.com>
2022-05-08 23:18:57 +0900

    .

--- client/stocklab-react/src/components/CodeSearch.js
+++ client/stocklab-react/src/components/CodeSearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Select from 'react-select';
+import makeAnimated from 'react-select/lib/animated';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import Radio from '@material-ui/core/Radio';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
